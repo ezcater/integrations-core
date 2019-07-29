7fb5f863b4cfb3a8e8e0c34b589b357bcffe9655
Ofek Lev <ofekmeister@gmail.com>
2019-07-29 15:44:09 -0400

    Implement configuration specifications

---
+++ datadog_checks_dev/datadog_checks/dev/tooling/configuration/utils.py
@@ -0,0 +1,7 @@
+# (C) Datadog, Inc. 2019
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
+
+def default_option_example(option_name):
+    return '<{}>'.format(option_name.upper())
